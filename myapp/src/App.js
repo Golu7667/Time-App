617b338a331e1e7b3d2369418e8beef42d6d7765
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-07 07:23:13 +0000

    updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,62 +1,38 @@
+import React, { Component } from 'react';
+import './App.css'
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
 
+  componentDidMount() {
+    // Update the time every second
+    this.timerID = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+  }
 
+  componentWillUnmount() {
+    // Clear the interval when the component is unmounted
+    clearInterval(this.timerID);
+  }
 
-
-
-
-
-
-
-      
-      
-          
-         
-         
-        
-          
-      
-
-// components/ResumeScanner.js
-
-import { useState } from 'react';
-
-export default function ResumeScanner() {
-  const [result, setResult] = useState('');
-
-  const handleFileUpload = async (e) => {
-    e.preventDefault();
-    const file = e.dataTransfer.files[0];
-    
-    // Perform file upload and scanning here
-    const text = await extractTextFromResume(file);
-    const isFullStack = checkFullStack(text);
-
-    setResult(isFullStack ? 'Full-stack developer' : 'Not a full-stack developer');
-  };
-
-  const extractTextFromResume = async (file) => {
-    // Implement text extraction logic from the uploaded resume (e.g., using a library like pdf-parse)
-    return 'Text content from the resume';
-  };
-
-  const checkFullStack = (text) => {
-    // Implement logic to check if the text contains keywords like "full stack"
-    return text.toLowerCase().includes('full stack');
-  };
-
-  return (
-    <div>
-      <h1>Resume Scanner</h1>
-      <div
-        onDrop={handleFileUpload}
-        onDragOver={(e) => e.preventDefault()}
-        style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center', cursor: 'pointer' }}
-      >
-        Drag and drop your resume here
+  render() {
+    return (
+      <div className='main'>
+      <div className='sub'>
+           <h1>Time</h1>
+            <p>{this.state.time}</p>
+            </div>   
       </div>
-      <button onClick={handleFileUpload}>Scan Resume</button>
-      <div>{result}</div>
-    </div>
-  );
+    );
+  }
 }
+
+export default App;
