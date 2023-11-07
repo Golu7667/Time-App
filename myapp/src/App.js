640c4b73b778a03b07574691be076d8ef863e89d
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-07 02:22:07 +0000

    home page update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+
+
+
+
+
+
+
+      
+      
+          
+         
+         
+        
+          
+        
+
+// components/ResumeScanner.js
+
+import { useState } from 'react';
+
+export default function ResumeScanner() {
+  const [result, setResult] = useState('');
+
+  const handleFileUpload = async (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    
+    // Perform file upload and scanning here
+    const text = await extractTextFromResume(file);
+    const isFullStack = checkFullStack(text);
+
+    setResult(isFullStack ? 'Full-stack developer' : 'Not a full-stack developer');
+  };
+
+  const extractTextFromResume = async (file) => {
+    // Implement text extraction logic from the uploaded resume (e.g., using a library like pdf-parse)
+    return 'Text content from the resume';
+  };
+
+  const checkFullStack = (text) => {
+    // Implement logic to check if the text contains keywords like "full stack"
+    return text.toLowerCase().includes('full stack');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Resume Scanner</h1>
+      <div
+        onDrop={handleFileUpload}
+        onDragOver={(e) => e.preventDefault()}
+        style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center', cursor: 'pointer' }}
+      >
+        Drag and drop your resume here
+      </div>
+      <button onClick={handleFileUpload}>Scan Resume</button>
+      <div>{result}</div>
     </div>
   );
 }
-
-export default App;
